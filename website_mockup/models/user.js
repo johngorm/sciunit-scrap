0add068237f241a1a91551f4a8f16bcf977038f1
JohnGorm <jack.m.gman@gmail.com>
2017-07-14 10:16:54 -0500

    Use bcrypt to hash password

--- website_mockup/models/user.js
+++ website_mockup/models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt');
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: {
@@ -19,10 +20,21 @@ module.exports = function(sequelize, DataTypes) {
 	}
   }, {
     classMethods: {
-      associate: function(models) {
+    	associate: function(models) {
         // associations can be defined here
       }
     }
+  }, {
+  	instanceMethods: {
+  		generateHash: function(password){
+  			return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+  		},
+
+  		validPassword: function(password){
+  			return bcrypt.compareSync(password, this.password);
+  		}
+
+  	}
   });
   return User;
 };
